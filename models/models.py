4d143602e5debe8e2a1c117e2cc7aac193d8506f
Nandy <nandhu02111997@gmail.com>
2022-11-11 20:38:45 -0500

    custom query implementation - model(MVC)
    Creating an first cut custom query imp which takes in a variable and runs a parametrized query.

---
+++ models/models.py
@@ -0,0 +1,29 @@
+from All_Players import All_Players
+from Career_Stats import Career_Stats
+from Active_Players import Active_Players
+from Season_Stats import Season_Stats
+from Teams import Teams
+from flask import *
+import sqlite3
+
+class San : 
+    def __init__(self):
+        # self.allplayers = 
+        pass
+
+    def find_player_names(self,player_name): 
+        conn = sqlite3.connect("/Users/nandy/Git_repos/APP-Project/NBA_database.db")
+        cur = conn.cursor()
+        query = 'SELECT  B.full_name, A.PTS from Career_Stats AS A JOIN All_Players as B where B.full_name == "{}" and B.id = A.PLAYER_ID;'.format(player_name)
+        cur.execute(query)
+        records = cur.fetchall()
+        print(records)
+        # return 
+
+
+
+if __name__ == "__main__": 
+    s1 = San()
+    s1.find_player_names("Stephen Curry")
+
+
