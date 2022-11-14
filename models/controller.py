520ed0d9c850052fe0c0fc2d9938c19dbbd5f6bc
Nandy <nandhu02111997@gmail.com>
2022-11-14 10:11:22 -0500

    Endpoints for all queries updated along with their HTML+CSS scripts

--- models/controller.py
+++ models/controller.py
@@ -1,29 +1,67 @@
 from flask import *
 from models import San 
+
+# import sys
+# sys.path.append('/Users/nandy/Git_repos/APP-Project/models/')
 # app = Flask(__name__, template_folder="new_folder_name")
+
 #move controllers to a folder and keep app.run() outside in a seperate folder and import app line down below 
 app = Flask(__name__)
 
 obj = San()
 
-
+#Home-Page
 @app.route("/")
 def start_(): 
     return render_template("homepage.html")
 
-@app.route("/player_stats")
+#Query List Page
+@app.route("/player_points")
 def player_stat(): 
-    return render_template("player_stats.html")
+    return render_template("player_points.html")
 
-@app.route("/form_player_stats", methods = ["GET","POST"])
-def form_player_stat(): 
+#Q1 - Results Page 
+@app.route("/form_player_points", methods = ["GET","POST"])
+def form_player_points(): 
     name = request.form["player_name"]
     rows = obj.find_player_names(name)
+    if len(rows) > 0 : 
+        return render_template("s_player_points.html", rows=rows)
+    else : 
+        return render_template("failure_page.html")
+
+#Q2 - Results Page
+@app.route("/active_players", methods = ["GET","POST"])
+def active_players(): 
+    rows = obj.gsw_active_players()
+    return render_template("s_active_players.html", rows=rows)
+
+#Q3 - Results Page     
+@app.route("/form_player_filter", methods = ["GET","POST"])
+def form_player_filter(): 
+    points = request.form["total_points"]
+    rows = obj.gsw_points_filter(points)
     print(rows, type(rows))
     if len(rows) > 0 : 
-        return render_template("s_playerstats.html", rows=rows)
+        return render_template("s_player_filter.html", rows=rows)
     else : 
-        return render_template("failure_page.html",)
+        return render_template("failure_page.html")
+
+#Q4 - Results Page 
+@app.route("/blocks_total", methods = ["GET","POST"])
+def blocks_total(): 
+    rows = obj.blocks_leader()
+    return render_template("s_blocks_leader.html", rows=rows)
+
+
+@app.route("/fg_percent_filter", methods = ["GET","POST"])
+def form_player_fgs(): 
+    fg_percent = request.form["fg_percent"]
+    rows = obj.fg_percent_filter(fg_percent)
+    print(rows, type(rows))
+    if len(rows) > 0 : 
+        return render_template("s_fg_percent.html", rows=rows)
+    else : 
+        return render_template("failure_page.html")
 
 app.run(debug=True, port=5001)
-
--- models/models.py
+++ models/models.py
@@ -1,32 +1,88 @@
-from All_Players import All_Players
-from Career_Stats import Career_Stats
-from Active_Players import Active_Players
-from Season_Stats import Season_Stats
-from Teams import Teams
-from flask import *
 import sqlite3
 
-class San : 
+from Active_Players import Active_Players
+from All_Players import All_Players
+from Career_Stats import Career_Stats
+from flask import *
+from Season_Stats import Season_Stats
+from Teams import Teams
+
+
+class San:
+    """
+    """
+
     def __init__(self):
-        # self.allplayers = 
+        # self.allplayers =
         pass
 
-    def find_player_names(self,player_name): 
+    def find_player_names(self, player_name):
+        """
+        method
+        """
         # conn = sqlite3.connect("D:/My Important Documents/Activities in Concordia University/SOEN 6441_APP_Fall2022/APP-Project/NBA_database.db")
-        conn = sqlite3.connect("/Users/nandy/Git_repos/APP-Project/NBA_database.db")
-        
+        conn = sqlite3.connect(
+            "/Users/nandy/Git_repos/APP-Project/NBA_database.db")
+
         cur = conn.cursor()
-        query = 'SELECT  B.full_name, A.PTS from Career_Stats AS A JOIN All_Players as B where B.full_name == "{}" and B.id = A.PLAYER_ID;'.format(player_name)
+        query = 'SELECT  B.full_name, A.PTS from Career_Stats AS A JOIN All_Players as B where B.full_name == "{}" and B.id = A.PLAYER_ID;'.format(
+            player_name)
         cur.execute(query)
         records = cur.fetchall()
-        print(records)
         return records
 
+    def gsw_active_players(self):
+        """
+        method
+        """
+        conn = sqlite3.connect(
+            "/Users/nandy/Git_repos/APP-Project/NBA_database.db")
+        cur = conn.cursor()
+        query = 'SELECT A.full_name from Active_Players as A where A.id in (SELECT PLAYER_ID from Season_Stats where TEAM_ABBREVIATION = "GSW" );'
+        cur.execute(query)
+        records = cur.fetchall()
+        return records
 
+    def gsw_points_filter(self, points):
+        """
+        method
+        """
+        conn = sqlite3.connect(
+            "/Users/nandy/Git_repos/APP-Project/NBA_database.db")
+        cur = conn.cursor()
+        query = "SELECT B.full_name, A.PTS from All_Players as B, Career_Stats AS A where B.id = A.PLAYER_ID and A.PTS > {};".format(
+            points)
+        cur.execute(query)
+        records = cur.fetchall()
+        return records
 
-if __name__ == "__main__": 
+    def blocks_leader(self):
+        """
+        method
+        """
+        conn = sqlite3.connect(
+            "/Users/nandy/Git_repos/APP-Project/NBA_database.db")
+        cur = conn.cursor()
+        query = 'SELECT  B.full_name, A.BLK from Career_Stats AS A JOIN All_Players as B where B.id = A.PLAYER_ID and A.BLK = (SELECT MAX(BLK) from Career_Stats);'
+        cur.execute(query)
+        records = cur.fetchall()
+        return records
+
+    def fg_percent_filter(self, fg_percent):
+        """
+        method
+        """
+        conn = sqlite3.connect(
+            "/Users/nandy/Git_repos/APP-Project/NBA_database.db")
+        cur = conn.cursor()
+        query = "SELECT A.PLAYER_ID, B.full_name, A.FG_PCT from All_Players as B, Season_Stats AS A WHERE (B.id = A.PLAYER_ID and A.FG_PCT > {} and A.SEASON_ID = '2020-21')  ORDER BY A.FG_PCT  DESC;".format(
+            fg_percent)
+        cur.execute(query)
+        records = cur.fetchall()
+        return records
+
+if __name__ == "__main__":
     s1 = San()
     r = s1.find_player_names("Stephen Curry")
     print(type(r))
     print(r)
-
